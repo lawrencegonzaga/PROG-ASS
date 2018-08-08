3de9c4a3d6c3dc3139228dc035f7d90ae4dc50a5
lawrencegonzaga <42152503+lawrencegonzaga@users.noreply.github.com>
2018-08-08 22:44:45 +0800

    Add files via upload

---
+++ howtocookrice.cpp
@@ -0,0 +1,27 @@
+#include <iostream> 
+using namespace std;
+ int main()
+    {
+     cout<<"How to Cook Rice"<< endl;
+  cout<<" " << endl;
+     cout<<"step 1 " << endl;
+     
+     cout<<"Take 4 cups of rice into the pot." << endl; 
+    cout<<"step 2 " << endl;
+    cout<<"Wash the grains two times." << endl; 
+   cout<<"step 3 " << endl;
+     cout<<"Take 5 cups of water into the pot." << endl; 
+   cout<<"step 4 " << endl;
+     cout<<"Dry the outside part of the pot." << endl;
+    cout<<"step 5 " << endl;
+     cout<<"Put the pot into the rice cooker." << endl; 
+   cout<<"step 6 " << endl;
+     cout<<"Plug the rice cooker." << endl;
+    cout<<"step 7" << endl;
+     cout<<"Press the button to turn on the rice cooker." << endl;
+   cout<<"step 8 " << endl;
+      cout<<"Wait for it to be cooked." << endl;
+    cout<<" " << endl;
+    cout<<"END " << endl;
+     return 0; 
+    }
